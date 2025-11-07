40c31b7fdcb7ca40ade7787a9562d3e99944da99
Puff <46506971+dchecketts@users.noreply.github.com>
2025-11-07 16:54:48 -0700

    Add Tailwind Typography and update CSS build output
    Integrated @tailwindcss/typography plugin and updated Tailwind build scripts to output CSS to src/styles/tailwind.css. Adjusted Eleventy passthrough to serve the new CSS file and enabled prose styling for markdown content.

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "src/_includes/header.html": "/header.html" });
     eleventyConfig.addPassthroughCopy({ "src/_includes/footer.html": "/footer.html" });
     eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy({ "src/styles/tailwind.css": "/style.css" });
 
     // This creates a "collection" of all files in the "posts" folder
     eleventyConfig.addCollection("posts", function (collectionApi) {
