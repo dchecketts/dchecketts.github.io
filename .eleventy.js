ef90fa45404d48207a8b60d7d36e3f6667d57619
Puff <46506971+dchecketts@users.noreply.github.com>
2025-11-07 12:46:51 -0700

    Add Tailwind CSS integration and image passthrough
    Integrated Tailwind CSS by adding tailwind.config.js and input.css, updating package.json with Tailwind dependencies, and modifying .eleventy.js to passthrough the images directory and fix src/href links. Updated base.html to support Tailwind styles.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy({ "src/_includes/header.html": "/header.html" });
     eleventyConfig.addPassthroughCopy({ "src/_includes/footer.html": "/footer.html" });
+    eleventyConfig.addPassthroughCopy("src/images");
 
     // This creates a "collection" of all files in the "posts" folder
     eleventyConfig.addCollection("posts", function (collectionApi) {
@@ -19,7 +20,23 @@ module.exports = function (eleventyConfig) {
             day: 'numeric',
             timeZone: 'UTC'
         });
-      });
+    });
+
+    eleventyConfig.addTransform("fix-src-links", function (content, outputPath) {
+        // Only run this transform on HTML files
+        if (outputPath && outputPath.endsWith(".html")) {
+
+            // Find all instances of src="/src/ and href="/src/
+            // and replace them with just src="/ and href="/
+            let newContent = content.replace(/(src|href)="\/(src)\//g, '$1="/');
+
+            return newContent;
+        }
+
+        // Return the content unchanged for other file types
+        return content;
+    });
+    // 👆 END OF NEW BLOCK 👆
 
     return {
         dir: {
@@ -29,4 +46,4 @@ module.exports = function (eleventyConfig) {
         },
         htmlTemplateEngine: "njk"
     };
-  };
+};
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+        './src/**/*.{njk,md,html}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+  };
