85ced84c8e1f68306c7b584236849aa4af837bca
Daniel Checketts <danielkchecketts@gmail.com>
2026-05-22 13:21:46 -0600

    Add: Content for Markdown examples. TODO: Components to add missing markdown features and beyond TODO: Content for the lessons

---
+++ src/lib/pages.ts
@@ -0,0 +1,10 @@
+export const pages = [
+    { url: '/blog/5202026', title: 'Your First Post Title' },
+    { url: '/learn/html', title: 'HTML Basics' },
+    { url: '/learn/css', title: 'CSS Guide' },
+    { url: '/learn/javascript', title: 'JavaScript Fundamentals' },
+    { url: '/learn/python', title: 'Python Guide' },
+    { url: '/learn/c-sharp', title: 'C# Basics' },
+    { url: '/learn/web-bonus', title: 'Web Bonus Content' },
+    // Add all your pages here
+];
