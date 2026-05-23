826c740e429b24033e3c144b213c53f4d34bb7af
Daniel Checketts <danielkchecketts@gmail.com>
2026-05-23 10:35:42 -0600

    Added majority of content to reach v1 stage :D

--- src/lib/pages.ts
+++ src/lib/pages.ts
@@ -1,5 +1,5 @@
 export const pages = [
-    { url: '/blog/5202026', title: 'Your First Post Title' },
+    {url: '/blog', title: 'Blog'},
     { url: '/learn/html', title: 'HTML Basics' },
     { url: '/learn/css', title: 'CSS Guide' },
     { url: '/learn/javascript', title: 'JavaScript Fundamentals' },
@@ -7,5 +7,7 @@ export const pages = [
     { url: '/learn/c-sharp', title: 'C# Basics' },
     { url: '/learn/web-bonus', title: 'Web Bonus Content' },
     { url: '/learn/html/what-can-do', title: 'What can I do with HTML Elements?' },
+    { url: '/blog/5202026', title: 'A New Site! (Again...)' },
+    { url: '/blog/5232026', title: 'Version 1.0' },
     // Add all your pages here
 ];
