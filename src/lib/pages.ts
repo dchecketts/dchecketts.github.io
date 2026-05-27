94a2e2accf1a40a02b5e808a2ef90171f707bc5e
Daniel Checketts <danielkchecketts@gmail.com>
2026-05-27 17:26:25 -0600

    Updated lots of content

--- src/lib/pages.ts
+++ src/lib/pages.ts
@@ -1,5 +1,5 @@
 export const pages = [
-    {url: '/blog', title: 'Blog'},
+    { url: '/blog', title: 'Blog'},
     { url: '/learn/html', title: 'HTML Basics' },
     { url: '/learn/css', title: 'CSS Guide' },
     { url: '/learn/javascript', title: 'JavaScript Fundamentals' },
@@ -9,5 +9,5 @@ export const pages = [
     { url: '/learn/html/what-can-do', title: 'What can I do with HTML Elements?' },
     { url: '/blog/5202026', title: 'A New Site! (Again...)' },
     { url: '/blog/5232026', title: 'Version 1.0' },
-    // Add all your pages here
+    { url: '/learn/html/getting-started', title: 'Getting Started'},
 ];
